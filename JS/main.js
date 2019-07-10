067ced41d81b125aa7ce3feb056207abf2116384
NicDominguez <NicDominguez@users.noreply.github.com>
2019-07-10 15:09:32 -0700

    Light Gallery Impletmented

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+// Calling Light Gallery Plugin
+
+$('document').ready(function () {
+    $('#lightgallery').lightGallery({
+        //Plugin Options here,
+        width: '60%',
+        backdropDuration: 300,
+        preload: 2,
+        subHtmlSelectorRelative: true,
+    });
+});
+
+// Calling Filterizr Plugin
